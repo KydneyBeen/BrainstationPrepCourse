f63ba80d860330e37751781a6223b0bb2cafe791
KydneyBean <kydney.been@gmail.com>
2018-04-22 10:00:06 -0700

    Assignment 1 - Basic JavaScript

---
+++ assignment-1.js
@@ -0,0 +1,8 @@
+var numbers = new Array();
+var count = 9;
+for (i = 0; i <= count; i++)
+    numbers.push(i);
+console.log("Array first number: " + numbers[0] + " Array last number: " + numbers[i-1])
+
+var car = new Object(colour)
+car.colour = "blank"
